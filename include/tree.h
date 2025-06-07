848b56e6c834550b2bf294d4bf62209875f4e437
Andrei <140070906+andrr72@users.noreply.github.com>
2025-06-07 16:48:34 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -32,4 +32,7 @@ class PMTree {
                    int target, std::vector<char>& result);
 };
 
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
 #endif  // INCLUDE_TREE_H_
