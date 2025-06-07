aa7bb618e1b07080b26e4589d218fca3b8b398aa
Andrei <140070906+andrr72@users.noreply.github.com>
2025-06-07 16:57:25 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -30,9 +30,9 @@ class PMTree {
   void getPermutations(Node* node, std::vector<char>& current,
                       std::vector<std::vector<char>>& result);
   void getPermByIndex(Node* node, std::vector<char>& current, int& index,
-                      int target, std::vector<char>& result);
+                      int target, std::vector<char>& result, int depth = 0);
   void navigateTree(Node* node, std::vector<char>& current, int& index,
-                   int target, std::vector<char>& result);
+                   int target, std::vector<char>& result, int depth = 0);
 };
 
 std::vector<char> getPerm1(PMTree& tree, int num);
